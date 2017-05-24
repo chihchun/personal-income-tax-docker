3193d9eddde151f5fe06d8c3cc635d2242596c84
shunyi <M157q.tw@gmail.com>
2017-05-24 13:40:11 +0800

    Open new tab for health insurance card in firefox.

--- 2017/firefox.sh
+++ 2017/firefox.sh
@@ -3,5 +3,6 @@ set -x
 sudo /etc/init.d/pcscd start
 firefox \
     -new-tab -url /usr/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/hipki@chttl.com.tw.xpi \
-    -new-tab -url https://rtn.tax.nat.gov.tw/ircweb/index.jsp
+    -new-tab -url https://rtn.tax.nat.gov.tw/ircweb/index.jsp \
+    -new-tab -url https://cloudicweb.nhi.gov.tw/cloudic/system/SMC/mEventesting.htm
 sudo /etc/init.d/pcscd stop
