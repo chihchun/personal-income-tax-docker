7609fc990f13392d77aa2272e5c7b931bb92d39a
Rex Tsai (蔡志展) <rex.cc.tsai@gmail.com>
2017-05-29 00:47:46 +0800

    Add COMODO RSA Organization Validation Secure Server CA (SHA-2) for the NHIICC daemon.

--- 2017/start.sh
+++ 2017/start.sh
@@ -10,7 +10,6 @@ sudo su -c "/tmp/health_insurance_card/mLNHIICC_Setup/mLNHIICC start"
 
 firefox \
     -new-tab -url /usr/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/hipki@chttl.com.tw.xpi \
-    -new-tab -url https://rtn.tax.nat.gov.tw/ircweb/index.jsp \
-    -new-tab -url https://cloudicweb.nhi.gov.tw/cloudic/system/SMC/mEventesting.htm
+    -new-tab -url https://rtn.tax.nat.gov.tw/ircweb/index.jsp
 
 sudo /etc/init.d/pcscd stop
