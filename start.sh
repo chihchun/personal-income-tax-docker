d137ae0c21d51030518fd687a5b8844ff0cbd5f2
ifurther <55025025+ifurther@users.noreply.github.com>
2022-08-20 23:28:30 +0800

    combine some apt line and update mLNHIICC version to 20220110

--- start.sh
+++ start.sh
@@ -8,13 +8,13 @@ sudo pcscd -d -f | tee /tmp/pcscd.log &
 cd /usr/local/HiPKILocalSignServerApp && ./start.sh &
 
 # Start plugin for health insurance card
-sudo /usr/local/share/NHIICC/mLNHIICC
+sudo /usr/local/share/NHIICC/mLNHIICC &
+sudo /usr/local/share/NHIICC/nhiicc --CertFile /usr/local/share/NHIICC/cert/NHIServerCert.crt --PrivateFileKey /usr/local/share/NHIICC/cert/NHIServerCert.key &
 
 firefox \
     --profile ${HOME}/.mozilla/firefox/default \
     https://github.com/chihchun/personal-income-tax-docker/blob/master/README.md \
-    https://emask.taiwan.gov.tw/msk/index.jsp \
     https://efile.tax.nat.gov.tw/irxw/index.jsp \
-    https://iccert.nhi.gov.tw:7777 \
+    https://localhost:7777 \
     https://cloudicweb.nhi.gov.tw/cloudic/system/SMC/webtesting/SampleY.aspx
     
